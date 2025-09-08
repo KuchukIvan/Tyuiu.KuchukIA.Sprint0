425bebda0418573a9d08cd37f5b535b5417c8715
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-09-08 20:26:39 +0500

    Task2

---
+++ Tyuiu.KuchukIA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KuchukIA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KuchukIA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Иван";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Иван", res);
+        }
+      
+    }
+}
