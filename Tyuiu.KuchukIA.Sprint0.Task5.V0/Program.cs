c7cbe83b977b2e462694e447193164baa3f4184e
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-09-09 21:30:15 +0500

    Task5

---
+++ Tyuiu.KuchukIA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KuchukIA.Sprint0.Task5.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("A + B = " + DataService.Addition(5, 5));
+
+        Console.WriteLine("A - B = " + DataService.Subtraction(5, 5));
+
+        Console.WriteLine("A * B = " + DataService.Multiplication(5, 5));
+
+        Console.WriteLine("A / B = " + DataService.Division(9, 3));
+
+        Console.ReadKey();
+    }
+}
