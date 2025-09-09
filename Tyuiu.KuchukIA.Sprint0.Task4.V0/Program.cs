ab6bcb160fd10ba5d14cff7cb2b18d4297ea17f4
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-09-09 18:48:41 +0500

    Task4

---
+++ Tyuiu.KuchukIA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KuchukIA.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(5, 5));
+
+        Console.WriteLine(DataService.Subtraction(5, 5));
+
+        Console.WriteLine(DataService.Multiplication(5, 5));
+
+        Console.WriteLine(DataService.Division(5, 5));
+
+        Console.ReadKey();
+    }
+}
